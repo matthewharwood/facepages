398a5acc0f4542a5bf083e936ce9a8b299a0296f
Matthew Harwood <matthhar12@gmail.com>
2015-01-26 11:24:09 -0800

    added the /feed route tot eh navigation bar

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -4,7 +4,7 @@ angular.module('facepagesApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
       'title': 'Home',
-      'link': '/'
+      'link': '/feed'
     }];
 
     $scope.isCollapsed = true;
