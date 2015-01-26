128d59b8a094f105fea6f2eae8f2bbf20379ac45
Matthew Harwood <matthhar12@gmail.com>
2015-01-26 15:29:34 -0800

    finished the flex box center capture template

--- client/app/account/signup/capture/capture.directive.js
+++ client/app/account/signup/capture/capture.directive.js
@@ -1,14 +1,46 @@
 'use strict';
 
 angular.module('facepagesApp')
-  .directive('capture', function () {
+  .directive('capture', function ($timeout) {
     return {
       templateUrl: 'app/account/signup/capture/capture.html',
       restrict: 'EA',
       controller: function($scope){
+        $scope.set = {};
         $scope.counter = [1,2,3];
+        $scope.set.captureHeight = {
+          height: (($('.inner-capture').width())/2) + 'px'
+        };
+        
+        console.log($scope.set.captureHeight);
+        navigator.getUserMedia =    navigator.getUserMedia       ||
+                                /* chrome и safari           */
+                                navigator.webkitGetUserMedia ||
+                                /* firefox                   */
+                                navigator.mozGetUserMedia    ||
+                                /* ie                        */
+                                navigator.msGetUserMedia;
+
+        window.URL = window.URL || window.webkitURL;
+        navigator.getUserMedia({audio: true, video: true},
+        function(pLocalMediaStream){
+             /*
+              * создаём элемент Video,
+              * в который помещаем картинку с веб-камеры\
+              */
+             var lVideo = $("#video");
+             
+
+             
+             lVideo[0].autoplay = true;
+             lVideo[0].src = URL.createObjectURL(pLocalMediaStream);
+        },
+        function(pError) { /* если возникла ошибка - выводим её */
+             alert(pError);
+        });
       },
       link: function (scope, element, attrs) {
+
       }
     };
   });
