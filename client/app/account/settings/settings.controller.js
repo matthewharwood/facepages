b6c6a9c3a401ad6400c62a84b4d64feb3cee5a89
Matthew Harwood <matthhar12@gmail.com>
2015-01-27 12:47:34 -0800

    added inline editing

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -3,9 +3,37 @@
 angular.module('facepagesApp')
   .controller('SettingsCtrl', function ($scope, User, Auth) {
     $scope.errors = {};
+    $scope.currentUser = Auth.getCurrentUser();
 
+    $scope.inlineEdit = {
+      skills: false,
+      name: false,
+    };
+    $scope.editglyp = {
+      skills: false,
+      name: false
+    };
+    $scope.defaultSkills = 'Double click to add skills';
+    $scope.toggleEditIcon = function(type, bool){
+      console.log(type, bool);
+      $scope.editglyp[type] = bool;
+    };
+
+    $scope.toggleInlineEdit = function(type, bool){
+      $scope.inlineEdit[type] = bool;
+    };
+
+    $scope.commaseperator = function(val){
+      $scope.user.skills = val;
+    }
+
+    //this needs to be filled out
+    $scope.changeName = function(form) {
+
+    };
     $scope.changePassword = function(form) {
       $scope.submitted = true;
+
       if(form.$valid) {
         Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
         .then( function() {
